f65976e50a9f146b59d4c6be3645f47a255d026a
htweko01 <htweko9925@gmail.com>
2023-10-19 19:58:01 +0630

    install vue for admin panel

---
+++ admin-panel/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1 class="bg-red-700">{{ msg }}</h1>
+  <h2 class="bg-purple-600">Hello</h2>
+  
+</template>
+
+<style scoped>
+</style>
